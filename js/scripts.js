ba4462f0f7d7bc2d3e1210e8c9d5f6881c0ac76b
VictorKMaina <vk13runic@gmail.com>
2020-03-02 07:44:44 +0300

    add some js code and an order.html page

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function pizza(type, size, toppings, deliver) {
+  this.type = type;
+  this.size = size;
+  this.toppings = toppings;
+  this.deliver = deliver;
+}
+function drink(type, flavor, amount, deliver) {
+  this.type = type;
+  this.flavor = flavor;
+  this.amount = amount;
+  this.deliver = deliver;
+}
