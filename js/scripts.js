ecb16402f9d9e8c656b924c4bc5e91656ff0c3ac
VictorKMaina <vk13runic@gmail.com>
2020-03-03 10:01:32 +0300

    chang cart.html to calculate total

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,42 @@
 var order = [];
 
-function Pizza(type, size, toppings, quantity, crust) {
+function Pizza(type, size, toppings, quantity, crust, price) {
   this.type = type;
   this.size = size;
   this.toppings = toppings;
   this.crust = crust;
   this.quantity = quantity;
+  this.price = price;
+}
+Pizza.prototype.appendPrice = function () {
+  if (this.type == "Pepperoni") {
+    return 400;
+  } else if (this.type == "Hawaiian") {
+    return 350;
+  } else if (this.type == "BBQ Chicken") {
+    return 450;
+  } else if (this.type == "Beef Mince") {
+    return 400;
+  } else if (this.type == "Mushroom") {
+    return 450;
+  } else if (this.type == "Eggplant") {
+    return 400;
+  }
+}
+
+Pizza.prototype.getCost = function () {
+  var baseCost = this.appendPrice();
+  if (this.size == "large") {
+    baseCost *= 2.5;
+  } else if (this.size == "medium") {
+    baseCost *= 1.6;
+  } else if (this.size == "small") {
+    baseCost *= 1;
+  }
+  if (this.toppings == true) {
+    baseCost += 150;
+  }
+  return baseCost * parseInt(this.quantity);
 }
 
 Pizza.prototype.addToCart = function () {
@@ -13,20 +44,15 @@ Pizza.prototype.addToCart = function () {
   localStorage.setItem("order", JSON.stringify(order));
   Swal.fire({
     icon: "success",
-    text: this.quantity + " " + this.size + " " + this.type + " Pizza(s) added to cart"
+    text: this.quantity + " " + this.size + " " + this.type + " Pizza(s) added to cart",
+    footer: "<table class='table table-sm table-borderless'><tbody><tr><td class='text-left'>Cost</td><td class='text-right'>KES " + this.price + "</td></tr></tbody><table>"
   });
-}
-
-function Drink(type, flavor, amount, deliver) {
-  this.type = type;
-  this.flavor = flavor;
-  this.amount = amount;
 }
 
 $().ready(function () {
   $(".pizza").click(function (event) {
     var thisOrder = new Pizza($("h4", this).first().text(), "", "", "", "");
-    var random = "<h3>" + thisOrder.type + "</h3><form> <table class='table table-resposive table-borderless text-dark mx-auto'> <tr> <td class='text-left'> <label>Quantity</label> <input class='' type='number' id='quantity' value='1' min='1' style='width: 70px;' required> </td> <td class='text-right'> <label>Size</label> <select class='' id='size' required> <option value=''> -- </option> <option value='large'>Large</option> <option value='large'>Medium</option> <option value='large'>Small</option> </select> </td> </tr> <tr> <td class='text-left'> <label for='crust'>Crust Type</label> <select name='crust' id='crust'> <option value=''required>--</option> <option value='Thick'>Thick Crust</option> <option value='Thin'>Thin Crust</option> </select> </td> <td class='text-right'> <label for='toppings'>Extra toppings</label> <input type='checkbox' id='toppings'> </td> </tr> </table> <p id='error' class='text-danger'></p><button class='btn btn-primary pizza' type='button' id='proceed'>Add to Cart</button></form>";
+    var random = "<h3>" + thisOrder.type + "</h3><form> <table class='table table-resposive table-borderless text-dark mx-auto'> <tr> <td class='text-left'> <label>Quantity</label> <input class='' type='number' id='quantity' value='1' min='1' style='width: 70px;' required> </td> <td class='text-right'> <label>Size</label> <select class='' id='size' required> <option value=''> -- </option> <option value='large'>Large</option> <option value='medium'>Medium</option> <option value='small'>Small</option> </select> </td> </tr> <tr> <td class='text-left'> <label for='crust'>Crust Type</label> <select name='crust' id='crust'> <option value=''required>--</option> <option value='Thick'>Thick Crust</option> <option value='Thin'>Thin Crust</option> </select> </td> <td class='text-right'> <label for='toppings'>Extra toppings</label> <input type='checkbox' id='toppings'> </td> </tr> </table> <p id='error' class='text-danger'></p><button class='btn btn-primary pizza' type='button' id='proceed'>Add to Cart</button></form>";
     Swal.fire({
       html: random,
       showConfirmButton: false
@@ -36,6 +62,7 @@ $().ready(function () {
       thisOrder.toppings = (document.getElementById("toppings").checked == true) ? true : false;
       thisOrder.crust = $("#crust").val();
       thisOrder.quantity = $("#quantity").val();
+      thisOrder.price = thisOrder.getCost();
       if (thisOrder.size == "" || thisOrder.crust == "") {
         $("p#error").text("Invalid order.");
       } else {
@@ -44,20 +71,25 @@ $().ready(function () {
       };
     });
   });
-  // $("span#cart-num").text(JSON.parse(localStorage.order).length);
-  $("#testing").click(function () {
-    if (localStorage.order == undefined) {
-      $("#cart-items").html('<p class="text-center">You have not added anything to the cart.</p>');
+  $().ready(function () {
+    if (localStorage.getItem("order") == undefined) {
+      $("#cart-items").html('<p class="text-center">Your cart is empty.</p>');
     } else {
-      // $("#cart-items").text("");
+      $("span#cart-num").text(JSON.parse(localStorage.order).length);
       var order = JSON.parse(localStorage.order);
-      $("#cart-items").html('<table class="table table-sm table-borderless text-dark"><tbody></tbody>');
       order.forEach(function (item) {
-        var test = '<tr> <td class="text-left w-75">' + JSON.parse(item).quantity + " " + JSON.parse(item).size + " " + JSON.parse(item).type + '</td> <td class="text-right w-25">' + '</td> </tr>';
-        $("#cart-items").append(test);
+        var test = '<tr> <td class="text-left w-75">' + JSON.parse(item).quantity + " " + JSON.parse(item).size + " " + JSON.parse(item).type + ' Pizza(s)</td> <td class="text-right w-25">KES ' + JSON.parse(item).price + '</td> </tr>';
+        $("#insertItems").append(test);
       })
-      $("#cart-items").append('</tbody> </table>');
-      console.log(order);
     }
-  })
-});
+    var num = JSON.parse(order[0]).price;
+    var nums = [];
+    order.forEach(function (item) {
+      nums.push(num);
+    })
+    var total = nums.reduce(function (item, num) {
+      return item + num;
+    })
+    $("#total-amount").text("KES " + total + ".00");
+  });
+})
